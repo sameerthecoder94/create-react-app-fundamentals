395c3f878d1f9ca2c10f4c73dc195a0a0ed5702e
sameerthecoder94 <sameerthecoder@gmail.com>
2022-03-26 00:57:27 -0400

    rendered array elements in UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
+console.log(React);
 console.log('I am starting your application!');
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  // const stories = 'My story';
+
   console.log('I have just entered into App component!');
   const [state, setState] = useState('');
 
@@ -34,6 +56,15 @@ const App = () => {
       <button onClick={handleClick}>Click Me</button>
 
       <h2>Searching for: {state.a}</h2>
+
+      <hr />
+
+      {stories.map((item) => (
+        <div key={item.objectID}>
+          <h2>{item.title}</h2>
+          <p>{item.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
