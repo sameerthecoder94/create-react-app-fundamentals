88cc6e4526e42ca6870b3b3eec60602102990b6b
sameerthecoder94 <sameerthecoder@gmail.com>
2022-03-26 01:43:36 -0400

    created multiple components and separation of concern

--- src/App.js
+++ src/App.js
@@ -1,70 +1,31 @@
-import React, { useState, useEffect } from 'react';
-
-console.log(React);
-console.log('I am starting your application!');
+import { useState, useEffect } from 'react';
+import List from './components/List';
+import { stories } from './components/storiesData';
 
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  // const stories = 'My story';
-
-  console.log('I have just entered into App component!');
   const [state, setState] = useState('');
 
   useEffect(() => {
     localStorage.setItem('myCat', 'Hello I am from localstorage');
   });
 
-  console.log(state);
-
   const handleChange = (e) => {
-    console.log('I have just entered into handleChange function');
     setState(e.target.value);
-    console.log(state);
   };
 
   const handleClick = () => {
     setState('hello');
   };
 
-  console.log(state);
-  console.log('I am entering into JSX!');
-
   return (
     <div>
       <h1>Searching...</h1>
       <label htmlFor='search'>Search: </label>
       <input type='text' id='search' onChange={handleChange} />
-
       <button onClick={handleClick}>Click Me</button>
-
       <h2>Searching for: {state.a}</h2>
-
       <hr />
-
-      {stories.map((item) => (
-        <div key={item.objectID}>
-          <h2>{item.title}</h2>
-          <p>{item.author}</p>
-        </div>
-      ))}
+      <List stories={stories}>hello</List>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 // import AppTwo from './AppTwo';
-import AppThree from './AppThree';
+// import AppThree from './AppThree';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<AppTwo />, document.getElementById('root'));
-ReactDOM.render(<AppThree />, document.getElementById('root'));
+// ReactDOM.render(<AppThree />, document.getElementById('root'));
