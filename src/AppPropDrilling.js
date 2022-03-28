24f72e7103c022008930c453bcf73dd428e1d980
sameerthecoder94 <sameerthecoder@gmail.com>
2022-03-27 21:28:27 -0400

    prop drilling..remove function added

--- src/AppPropDrilling.js
+++ src/AppPropDrilling.js
@@ -1,29 +1,43 @@
 import { useState } from 'react';
 import { data } from './peopleData';
 
-const List = ({ people }) => {
-  console.log(people[0].id);
+const List = ({ people, removePerson }) => {
   return (
     <div>
       {people.map((people) => (
-        <SinglePerson key={people.id} people={people} />
+        <SinglePerson
+          key={people.id}
+          people={people}
+          removePerson={removePerson}
+        />
       ))}
     </div>
   );
 };
 
-const SinglePerson = ({ people }) => {
-  return <h3>{people.name}</h3>;
+const SinglePerson = ({ people, removePerson }) => {
+  return (
+    <>
+      <h3>{people.name}</h3>
+      <button onClick={() => removePerson(people.id)}>REMOVE</button>
+    </>
+  );
 };
 
 const App = () => {
   const [people, setpeople] = useState(data);
 
+  const removePerson = (id) => {
+    setpeople((people) =>
+      people.filter((person) => person.id !== id)
+    );
+  };
+
   return (
     <>
       <section>
         <h1 style={{ color: 'red' }}>Prop Drilling</h1>
-        <List people={people} />
+        <List people={people} removePerson={removePerson} />
       </section>
     </>
   );
